d619787969a47e427b93ce1b63d1277e09a35a36
Alpha-car <Alpha-car>
2021-11-24 04:08:01 +0900

    제목: TimeSchedule 화면 네비게이터 수정 * feature : 새로운 기능
    1. TimeSchedule 화면 네비게이터 Back 버튼 글자 지우기
    2. TimeSchedule 화면 네비게이터 우측에 Search 버튼 추가
    3. TimeSchedule 화면 중앙에 Back 버튼 만들어보기(이건 그냥 만듦,,,)

--- Front-end/LimeFront/App.js
+++ Front-end/LimeFront/App.js
@@ -1,5 +1,6 @@
 // App.js
 import React, {Component} from 'react';
+import {Button} from "react-native";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -14,7 +15,16 @@ export default class App extends Component {
             <NavigationContainer>
                 <Stack.Navigator>
                     <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-                    <Stack.Screen name="TimeSchedule" component={TimeSchedule}/>
+                    <Stack.Screen name="TimeSchedule" component={TimeSchedule} 
+                        options={{ 
+                            title: '시간표 추가',
+                            headerBackTitleVisible: false,
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => alert('검색창 띄울 예정,,,')}
+                                    title = "search"
+                                />
+                            )}} />
                 </Stack.Navigator>
             </NavigationContainer>
         );
--- Front-end/LimeFront/TimeSchedule.js
+++ Front-end/LimeFront/TimeSchedule.js
@@ -1,10 +1,29 @@
 import React, {Component} from "react";
-import {Text} from "react-native";
+import { View, Text, Button, navigation } from "react-native";
 
-export default class TimeSchedule extends Component {
-    render() {
-        return (
+export default function TimeSchedule({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>어,, 재밌네,,</Text>
             <Text>시간표</Text>
-        )
-    }
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+        </View>
+    );
 }
+
+
+
+
+// export default class TimeSchedule extends Component () => {
+//     render() {
+//         return (
+//             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//                 <Text>Profile screen</Text>
+//                 <Text>시간표</Text>
+//                 <Button title="Go back" onPress={() => navigation.goBack()} />
+//             </View>
+            
+            
+//         )
+//     }
+// }
