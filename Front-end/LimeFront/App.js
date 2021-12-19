1b0e41181d7a0df54455710dbbcc9cab12473f42
Alpha-car <88402715+Alpha-car@users.noreply.github.com>
2021-12-19 15:30:49 +0900

    Revert "2021_11_24 TimeSchedule 화면 네비게이터 작업"

--- Front-end/LimeFront/App.js
+++ Front-end/LimeFront/App.js
@@ -1,10 +1,9 @@
 // App.js
 import React, {Component} from 'react';
-import {Button} from "react-native";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Login from './Login';
+import Home from './home';
 import TimeSchedule from './TimeSchedule';
 
 const Stack = new createNativeStackNavigator();
@@ -12,19 +11,10 @@ const Stack = new createNativeStackNavigator();
 export default class App extends Component {
     render() {
         return (
-            <NavigationContainer independent={true}>
+            <NavigationContainer>
                 <Stack.Navigator>
-                    <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-                    <Stack.Screen name="TimeSchedule" component={TimeSchedule} 
-                        options={{ 
-                            title: '시간표 추가',
-                            headerBackTitleVisible: false,
-                            headerRight: () => (
-                                <Button
-                                    onPress={() => alert('검색창 띄울 예정,,,')}
-                                    title = "search"
-                                />
-                            )}} />
+                    <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+                    <Stack.Screen name="TimeSchedule" component={TimeSchedule}/>
                 </Stack.Navigator>
             </NavigationContainer>
         );
